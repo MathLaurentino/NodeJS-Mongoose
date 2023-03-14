1f1da4a65eb04426e9e7719032eb34a5a4cca185
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-03-14 13:40:52 -0300

    CRUD categorias completo

--- app.js
+++ app.js
@@ -4,24 +4,24 @@
      */
     const express = require("express");
     const app = express();
-    const handlebars = require("express-handlebars");
-    const bodyParser = require("body-parser");
-    const path = require("path");
-    const admin = require("./routes/admin");
-    const mongoose = require("mongoose");
-    const session = require("express-session");
-    const flash = require("connect-flash");
+    const handlebars = require("express-handlebars"); //motor de visualização de páginas
+    const bodyParser = require("body-parser"); // processamento de formulários
+    const mongoose = require("mongoose"); // conexão com o mongoDB
+    const session = require("express-session"); // criação de sessão do usuário
+    const flash = require("connect-flash"); // mensagens flash (só aparecem uma vez)
+    const path = require("path"); // vem com o nodeJS
+    const admin = require("./routes/admin"); // rotas admin
     
 
 // Config
     // Session
         /* Configuração da sessão */ 
         app.use(session({
-            secret: "aprendendonodejs",
-            resave: true,
-            saveUninitialized: true 
+            secret: "aprendendonodejs", // chave secreta utilizada para assinar o cookie de sessão e garantir que o usuário não possa alterar o seu valor.
+            resave: true, // indica se a sessão deve ser regravada mesmo que não tenha sido modificada durante a requisição 
+            saveUninitialized: true // indica se deve ser salvo uma sessão nova quando ela é criada, mesmo que ela ainda não tenha sido modificada.
         }));
-        app.use(flash());
+        app.use(flash()); // gerenciar mensagens de erro ou sucesso
 
     // Middleware 
         /* Middleware para mensagens de feedback ao usuário */
--- routes/admin.js
+++ routes/admin.js
@@ -17,12 +17,14 @@
         res.render("admin/index");
     });
 
+
     /**
     * Rota para renderizar a página de gerenciamento de posts do painel administrativo
     */
     router.get('/posts', (req, res) => {
         res.render("admin/index");
     });
+
 
     /**
      * Rota para renderizar a página de gerenciamento de categorias do painel administrativo
@@ -36,12 +38,14 @@
         });
     });
 
+
     /**
      * Rota para renderizar a página de adição de categoria do painel administrativo
      */
     router.get('/categorias/add', (req, res) => {
         res.render("admin/addcategoria");
     });
+
 
     /**
      * Rota para adicionar uma nova categoria
@@ -70,14 +74,68 @@
                 slug: req.body.slug
             }
             new Categoria(novaCategoria).save().then(() => {
-                req.flash("success_msg", "Categoria criada com sucesso!")
+                req.flash("success_msg", "Categoria criada com sucesso!");
                 res.redirect("/admin/categorias");
             }).catch((err) => {
                 req.flash("error_msg", "Erro ao salvar categoria!");
                 res.redirect("/admin");
-            })
+            });
         } 
     });
+
+
+    /**
+     * Rota para renderizar a página de edição de categoria do painel administrativo
+     */
+    router.get("/categorias/adit/:id", (req, res) => {
+        Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+            res.render("admin/editcategorias", {categoria: categoria});
+        }).catch((err) => {
+            req.flash("error_msg", "Essa categoria não existe");
+            res.redirect("/admin/categorias");
+        });
+    });
+
+
+    /**
+     * Rota para editar uma categoria
+     */
+    router.post("/categorias/edit", (req, res) => {
+
+        Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+            categoria.nome = req.body.nome;
+            categoria.slug = req.body.slug;
+
+            categoria.save().then(()=> {
+                req.flash("success_msg", "categoria editada com sucesso");
+                res.redirect("/admin/categorias");
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro interno ao salvar edição da categoria");
+                res.redirect("/admin/categorias");
+            });
+
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao editar a categoria");
+            res.redirect("/admin/categorias");
+        });
+    });
+
+
+    /**
+     * Rota para apagar uma categoria
+     */
+    router.post("/categorias/deletar", (req, res) => {
+        Categoria.deleteOne({_id: req.body.id }).then(() => {
+            req.flash("success_msg", "Categoria deletada com sucesso");
+            res.redirect("/admin/categorias");
+        }).catch((err) => {
+            req.flash("error_msg", "Falha ao deletar categoria");
+            res.redirect("/admin/categorias");
+        });        
+    });
+
+
 
 // Export
     module.exports = router;
